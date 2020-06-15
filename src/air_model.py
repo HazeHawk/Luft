e932767875055e3e0e6ce7422f017bdd3d19b823
Zailux <50146015+Zailux@users.noreply.github.com>
2020-06-15 20:16:29 +0200

    GeoJSON + Geo Queries examples

---
+++ src/air_model.py
@@ -0,0 +1,16 @@
+''' This is the air_model Module
+Contains all model classes for DB connects and datahandling.
+'''
+import datetime
+
+from src.config import Configuration
+
+
+class AirModel(object):
+
+    def __init__(self):
+        pass
+
+
+    def read(self):
+        pass
